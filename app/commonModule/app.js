a93ef4ff69837bacef3e46714d04f6edce36001e
QutantumProgrammer <zcfxs@hotmail.com>
2017-03-19 20:07:27 +0800

    first commit

---
+++ app/commonModule/app.js
@@ -0,0 +1,7 @@
+
+define(["angular", "jquery", "bootstrap"], function () {
+        
+        var app = angular.module("rankApp", []);
+        
+        return app;
+});
---
+++ app/rankModule/launcher.js
@@ -0,0 +1,8 @@
+require([ "../commonModule/config"],function(){
+	require(["../controller/mainController"],function(){
+		angular.bootstrap(document, ['rankApp']);
+	});		
+});
+
+
+
